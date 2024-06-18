4467881c161bce91bca557230a2560bfd142149d
NickSheva <nikshevtsov@mail.ru>
2024-06-18 15:02:07 +0500

    Add division and multiplication

--- file_4.py
+++ file_4.py
@@ -1,13 +1,22 @@
 import tensorflow as tf
-"""Вычисляется сумма, разница констант a и b, 
+"""Вычисляется сумма, разница, произведение и частное констант a и b, 
 созданных методом tf.constant()"""
 def main():
-   a = tf.constant(4)
-   b = tf.constant(5)
-   c = tf.add(a, b)
-   print(c.numpy())
-   s = tf.subtract(a, b)
+   x = tf.constant(4)
+   y = tf.constant(5)
+   a = tf.add(x, y)
+   print('Addition')
+   print(a.numpy())
+   s = tf.subtract(x, y)
+   print('Substracation')
    print(s.numpy())
+   m = tf.multiply(x, y)
+   print('Multiplication')
+   print(m.numpy())
+   d = tf.divide(x, y)
+   print('Division')
+   print(d.numpy())
+
 
 
 if __name__ == "__main__":
