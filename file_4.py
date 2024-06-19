0340e551a479a04398350bd813d82dab2efa8aba
NickSheva <nikshevtsov@mail.ru>
2024-06-19 14:49:36 +0500

    changed the session with tf.compat.v1.Session()

--- file_4.py
+++ file_4.py
@@ -1,23 +1,37 @@
 import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+import numpy as np
 """Вычисляется сумма, разница, произведение и частное констант a и b, 
 созданных методом tf.constant()"""
-def main():
-   x = tf.constant(4)
-   y = tf.constant(5)
-   a = tf.add(x, y)
-   print('Addition')
-   print(a.numpy())
-   s = tf.subtract(x, y)
-   print('Substracation')
-   print(s.numpy())
-   m = tf.multiply(x, y)
-   print('Multiplication')
-   print(m.numpy())
-   d = tf.divide(x, y)
-   print('Division')
-   print(d.numpy())
+
+# Create a constant tensor
+x = tf.constant(8)
+y = tf.constant(5)
+# make session
+sess = tf.compat.v1.Session()
+# find add
+a = tf.add(x, y)
+print(f'Addition: {sess.run(a)}')
+# find sub
+s = tf.subtract(x, y)
+print(f'Substracation: {sess.run(s)}')
+# find mul
+m = tf.multiply(x, y)
+print(f'Multiplication: {sess.run(m)}')
+# find div
+d = tf.divide(x, y)
+print(f'Division: {sess.run(d)}')
 
 
 
-if __name__ == "__main__":
-    main()
+# Create a constant tensor
+tensor = tf.constant([1, 2, 3])
+# Create a session
+session = tf.compat.v1.Session()
+# Run the session to find the first element
+result = session.run(tensor[0])
+# Print the result
+print(result)
+
+
+
