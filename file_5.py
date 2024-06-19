5571ae39bf2f537b04ccd856de2a8c5d213803d1
NickSheva <nikshevtsov@mail.ru>
2024-06-19 14:51:00 +0500

    add file_5

--- file_5.py
+++ file_5.py
@@ -1,6 +1,14 @@
-def main():
-    pass
-    
-    
-if __name__ == "__main__":
-    main()
+import gymnasium as gym
+
+
+env = gym.make("LunarLander-v2", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
